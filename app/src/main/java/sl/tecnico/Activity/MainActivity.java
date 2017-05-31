29ec59e1c4f965e9caf8c015790c7a1d6677ff4b
Sergio Senna <sergiosenna88@gmail.com>
2017-05-31 20:17:42 -0300

    Update Activity

--- app/src/main/java/sl/tecnico/Activity/MainActivity.java
+++ app/src/main/java/sl/tecnico/Activity/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import sl.tecnico.R;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
